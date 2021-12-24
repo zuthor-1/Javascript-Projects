b2c453e5f38a6675381dba806df09379de17278c
Abhay <Abhay831877@gmail.com>
2021-12-25 00:41:54 +0530

    birthday countdown added

---
+++ Birthday-Countdown/script.js
@@ -0,0 +1,44 @@
+
+const daysE = document.getElementById('day');
+const hoursE = document.getElementById('hour');
+const minsE= document.getElementById('min');
+const secondsE=document.getElementById('sec');
+
+
+const newyear = "10 Oct 2022";
+
+function format(time)
+{
+ return time < 10 ? `0${time}`: time; // to format the no with 00 form.
+}
+
+function countdown()
+{
+    const newYearsDate = new Date(newyear);
+    const currentDate = new Date();
+    
+    console.log(newYearsDate - currentDate);
+    
+    const totalseconds =(newYearsDate - currentDate )/1000;
+
+    const days = Math.floor(totalseconds/3600/24);
+    const hours = Math.floor(totalseconds/3600)%24;
+    const mins = Math.floor(totalseconds/60)%60;
+    const seconds = Math.floor(totalseconds)%60;
+
+    
+    
+    daysE.innerHTML    = format(days);
+    hoursE.innerHTML   = format(hours);
+    minsE.innerHTML    = format(mins);
+    secondsE.innerHTML = format(seconds);
+  
+}
+
+
+countdown();
+
+
+setInterval(countdown,1000);
+
+
